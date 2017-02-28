2d91c058055951e94cb9446b20fb1fca088e6547
DM\ehuds <ehud.shavit@checkmarx.com>
2017-02-28 22:56:33 +0200

    Mark CredentialsValidator as FunctionalInterface

--- CX-SDK-Domain/src/main/java/com/cx/sdk/domain/CredentialsValidator.java
+++ CX-SDK-Domain/src/main/java/com/cx/sdk/domain/CredentialsValidator.java
@@ -5,6 +5,7 @@ import com.cx.sdk.domain.exceptions.SdkException;
 /**
  * Created by ehuds on 2/26/2017.
  */
+@FunctionalInterface
 public interface CredentialsValidator {
     void validate(String userName, String password) throws SdkException;
 }
