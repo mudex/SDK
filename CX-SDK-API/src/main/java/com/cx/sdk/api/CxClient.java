c84ddec9240eca09b76b0b22bd66fe44eb003bc2
DM\orlyk <orly.kilidgan@checkmarx.com>
2017-04-25 17:12:38 +0300

    Add minimal java doc to the CxClient APIs & submit the classpath

--- CX-SDK-API/src/main/java/com/cx/sdk/api/CxClient.java
+++ CX-SDK-API/src/main/java/com/cx/sdk/api/CxClient.java
@@ -12,11 +12,35 @@ import java.util.List;
  * Created by ehuds on 2/22/2017.
  */
 public interface CxClient {
+	
+    /*
+     * @return SessionDTO
+     * @throws SdkException
+     */
     SessionDTO login() throws SdkException;
 
+    /*
+     * @return List<EngineConfigurationDTO>
+     * @throws CxClientException
+     */
     List<EngineConfigurationDTO> getEngineConfigurations() throws CxClientException;
+    
+    /*
+     * @return List<PresetDTO>
+     * @throws CxClientException
+     */
     List<PresetDTO> getPresets() throws CxClientException;
+    
+    /*
+     * @return List<TeamDTO>
+     * @throws CxClientException
+     */
     List<TeamDTO> getTeams() throws CxClientException;
-
+    
+    /*
+     * @param projectName
+     * @param teamId
+     * @throws CxClientException
+     */
     void validateProjectName(String projectName, String teamId) throws CxClientException;
 }
