3f953b0cd9f30f8e6149514427951c8ccc13d913
adig <adi.gido@checkmarx.com>
2018-12-04 13:36:46 +0200

    Revert: OIDC changes: - Remove kerberos authentication - Check token expiration

---
+++ CX-SDK-Infrastructure/src/main/java/com/cx/sdk/infrastructure/authentication/kerberos/WindowsAuthenticator.java
@@ -0,0 +1,21 @@
+package com.cx.sdk.infrastructure.authentication.kerberos;
+
+import waffle.util.Base64;
+import waffle.windows.auth.impl.WindowsSecurityContextImpl;
+
+/**
+ * Created by ehuds on 21/05/2017.
+ */
+public class WindowsAuthenticator {
+    public static final String securityPackage = "Negotiate";
+    public static String getKrbToken(String aTargetSPName) {
+        if(null == aTargetSPName || aTargetSPName.trim().isEmpty()){
+            return null;
+        }
+        return Base64.encode(WindowsSecurityContextImpl.getCurrent(securityPackage, aTargetSPName).getToken());
+    }
+
+    private WindowsAuthenticator(){
+        super();
+    }
+}
